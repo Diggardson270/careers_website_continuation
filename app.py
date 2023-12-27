c2697598ee04e95eea1e23fb101b31fd9f946779
Enoch Aniyikaye <97711219+Diggardson270@users.noreply.github.com>
2023-12-27 02:33:08 +0100

    made little changes

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+
+JOBS = [
+    {
+        'id' : 1,
+        'title' : 'Data Analyst',
+        'location' : 'Remote',
+        'salary' : 'N 2,000,000'
+    },
+    {
+        'id' : 2,
+        'title' : 'Data Engineer',
+        'location' : 'Abuja, Nigeria',
+        'salary' : 'N 5,000,000'
+    },
+    {
+        'id' : 3,
+        'title' : 'Chief of Operations',
+        'location' : 'Abuja, Nigeria',
+        'salary' : 'N 10,000,000'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello World, it is I</p>"
+    return render_template('home.html', jobs = JOBS, company_name = 'Enoch')
 
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
